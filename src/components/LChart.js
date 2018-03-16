69c96f873e2bbac82df15fef87e74cfe6710f8f1
Duc Nguyen <minhducng93@gmail.com>
2018-03-15 22:04:30 -0400

    Add chart js

--- src/components/LChart.js
+++ src/components/LChart.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import LineChart from 'react-linechart';
 import 'react-linechart/dist/styles.css';
+import jsonData from '../data.json';
+// import jsonTest from '../test.json';
  
 export default class LChart extends Component {
     constructor(props) {
         super(props);
-
+        this.getData = this.getData.bind(this);
+        this.state = {};
     }
+
+    componentDidMount() {
+        let dataPrice = jsonData;
+        // console.log(data1.data);
+        // let dataTest = JSON.parse(jsonTest);
+        this.setState({ dataPrice: dataPrice }, function() {
+            // console.log(this.state.dataPrice);
+        });
+      }
+
+    getData = (crypto, currency) => {
+        
+        let data = jsonData;
+        let jso = {...this.state.dataPrice};
+        console.log(data[crypto][currency]);
+        console.log(jso[crypto][currency]);
+        console.log(JSON.stringify(data) === JSON.stringify(this.state.dataPrice));
+
+        return data[crypto][currency];
+    }
+
     render() {
         let xAxis = "Year";
         let yAxis = this.props.y;
         let crypto = this.props.x;
-        const data = [
-            {									
-                color: "steelblue", 
-                points: [{x: 1, y: 2}, {x: 3, y: 5}, {x: 7, y: -3}] 
-            }
-        ];
+        let data = this.getData(this.props.x, this.props.y);
+        
         return (
             <div>
                 <div className="App">
