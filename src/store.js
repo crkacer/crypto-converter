741fffae91f8f2aa1a541a64d2d1d2c161b77ea7
Duc Nguyen <minhducng93@gmail.com>
2018-03-22 13:36:00 -0400

    Add store

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { createStore } from 'redux';
+
+// import the root reducer
+import rootReducer from './reducers/index';
+
+// create an object for the default data
+let crypto = ['BTC', 'ETH', 'XRB', 'BCH'];
+let currency = ['USD', 'EUR'];
+
+const defaultState = {
+  crypto,
+  currency
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export default store;
